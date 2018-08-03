630e8df397ce4a15e40c3bba1d847385f1e89772
Jaden <Jaden.johnson@hawkeyecollege.edu>
2018-08-03 04:34:56 -0500

    discord bot

--- src/main/java/us/lemin/core/discord/listeners/MessageListener.java
+++ src/main/java/us/lemin/core/discord/listeners/MessageListener.java
@@ -1,6 +1,7 @@
 package us.lemin.core.discord.listeners;
 
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -21,15 +22,24 @@ public class MessageListener extends ListenerAdapter {
                 case "ban":
                     String bannedId = new StringBuilder(message[1]).delete(0, 1).deleteCharAt(message[1].length()).toString();
                     String bannedIdMention = event.getJDA().getUserById(bannedId).getAsMention();
-                    String reason = message[2];
+                    String bannedReason = message[2];
                     event.getChannel().sendMessage(new BanEmbed(event.getJDA().getUserById(bannedId).getAsMention(),
                             event.getAuthor().getAsMention()).build()).queue();
                     event.getJDA().getUserById(bannedId).openPrivateChannel().queue((channel) ->
                     {
                         channel.sendMessage("You were banned by " + event.getAuthor().getName()
-                                + "##" + event.getAuthor().getDiscriminator() + " for " + reason).queue();
+                                + "##" + event.getAuthor().getDiscriminator() + " for " + bannedReason).queue();
                     });
                     event.getGuild().getController().ban(bannedId, 1, message[2]).queue();
+                    break;
+                case "mute":
+                    Role muted = event.getGuild().getRoleById("474776391678689280");
+                    String mutedId = new StringBuilder(message[1]).delete(0, 1).deleteCharAt(message[1].length()).toString();
+                    String mutedReason = message[2];
+                    event.getChannel().sendMessage(new BanEmbed(event.getJDA().getUserById(mutedId).getAsMention(),
+                            event.getAuthor().getAsMention()).build()).queue();
+                    event.getGuild().getMember(event.getJDA().getUserById(mutedId)).getRoles().add(muted);
+                    break;
             }
         }
     }
