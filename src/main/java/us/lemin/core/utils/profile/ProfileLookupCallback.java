8d0eabbb567dcea573970ccbbe4b4c78915ff54e
ilovefuud <jaden.johnson@hawkeyecollege.edu>
2019-04-05 19:54:11 -0500

    Merge branch 'master' of C:\Users\Jaden\IdeaProjects\Lemin Core with conflicts.

--- src/main/java/us/lemin/core/utils/profile/ProfileLookupCallback.java
+++ src/main/java/us/lemin/core/utils/profile/ProfileLookupCallback.java
@@ -1,5 +1,5 @@
 package us.lemin.core.utils.profile;
 
 public interface ProfileLookupCallback {
-    void callBack(ProfileUtil.MojangProfile result, boolean Success);
+    void callBack(ProfileUtil.MojangProfile result, boolean success);
 }
